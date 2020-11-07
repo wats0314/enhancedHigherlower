bc2c37f900615f0e3886635f2fa6b641e2db1039
unknown <wats0314@d.umn.edu>
2020-11-07 15:29:56 -0600

    getting back into into into

---
+++ Scripts/enhancedHigherlower.js
@@ -0,0 +1,16 @@
+
+
+let yourNumber = Math.round(prompt("enter a number", "0"))
+
+console.log("your number rounded = " + yourNumber)
+
+function validate(){
+    if (yourNumber == 43){alert ("we good")}
+    if (yourNumber <0){alert("refresh the page a positive integer")}
+    else {alert("refresh the page a positive integer")}
+}
+    
+    
+
+
+
