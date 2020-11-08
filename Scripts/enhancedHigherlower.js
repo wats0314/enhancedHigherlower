0da56f61ee9e6cdce68ddb979e5cea0217bef15c
unknown <wats0314@d.umn.edu>
2020-11-07 19:48:17 -0600

    validated prompts

--- Scripts/enhancedHigherlower.js
+++ Scripts/enhancedHigherlower.js
@@ -1,15 +1,21 @@
 
 
-let yourNumber = Math.round(prompt("enter a number", "0"))
+
+let yourNumber = Number(Math.round(prompt("Guess a number between 1 and (enter your choice)", "0")));
 
 console.log("your number rounded = " + yourNumber)
 
-function validate(){
-    if (yourNumber == 43){alert ("we good")}
-    if (yourNumber <0){alert("refresh the page a positive integer")}
-    else {alert("refresh the page a positive integer")}
+function checkNumber(){
+    while (isNaN(yourNumber)){
+        yourNumber = Number(Math.round(prompt("enter a number not words)", "0")));
+    }
 }
-    
+function greaterThan(){
+        while (yourNumber < 1){
+        yourNumber = Number(Math.round(prompt("Guess a number between 1 and (enter your choice)", "0")));
+        }
+}
+  
     
 
 
